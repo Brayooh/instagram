65a8cd274186cea7a75b390dbe8050b22200720c
brayooh <brianndichu.bn@gmail.com>
2020-03-10 15:52:12 +0300

    adds logout url

--- Pics/urls.py
+++ Pics/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('register/', views.registration, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='home'),
+    path('login/', views.logout_view, name='logout'),
     path('accounts/profile/', views.profile, name='profile'),
     path('posts/', views.post, name='posts'),
     path('post_create/', views.post_create, name="createpost"),
