bfca3a95f60045c3f273e9f1eef3dd41597421f1
brayooh <brianndichu.bn@gmail.com>
2020-03-22 04:55:47 +0300

    changes url pattern for authentication links

--- Pics/urls.py
+++ Pics/urls.py
@@ -1,22 +1,19 @@
-from django.urls import path, re_path
+from django.urls import path, include
 from . import views
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
+    path('accounts/',include("django.contrib.auth.urls")),
     path('register/', views.registration, name='register'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('login/', views.logout_view, name='logout'),
-    path('accounts/profile/', views.profile, name='profile'),
+    path('profile/', views.profile, name='profile'),
     path('', views.post, name='posts'),
     path('post_create/', views.post_create, name="createpost"),
     path('commenting/<post_id>', views.commenting, name="commenting"),
     path('comment/<post_id>/', views.comment, name="comment"),
     path('search/', views.search_user, name="search"),
     path('likes/<post_id>', views.likes, name="likes"),
-    re_path(r'^follow/(?P<operation>.+)/(?P<pk>\d+)/$', views.follow, name="follow" )
+    # re_path(r'^follow/(?P<operation>.+)/(?P<pk>\d+)/$', views.follow, name="follow" )
 ]
 
 if settings.DEBUG:
