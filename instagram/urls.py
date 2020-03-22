99789b4ebf60b401e21868d2de0746cd68f23ae5
brayooh <brianndichu.bn@gmail.com>
2020-03-22 05:00:17 +0300

    adds path to .auth.urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,4 +21,5 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Pics.urls')),
+    path('',include("django.contrib.auth.urls"))
 ]
