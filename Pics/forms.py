3e3caedc9b3fff56419615ec1575754bc3d3a89a
brayooh <brianndichu.bn@gmail.com>
2020-04-03 00:28:59 +0300

    fixes bugs

--- Pics/forms.py
+++ Pics/forms.py
@@ -31,4 +31,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image', 'bio']
+        fields = ['profile_image', 'bio']
