528d8b92d0d08194d47b45191531d33515dc7d7b
brayooh <brianndichu.bn@gmail.com>
2020-04-03 01:32:59 +0300

    fixes bugs for deploymnt

--- instagram/settings.py
+++ instagram/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
     'default': {
